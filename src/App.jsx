d7a238286c69cda9fe96136b94b93e95260e186e
tainguyencoder <tainguyencoder@gmail.com>
2024-09-07 17:29:41 +0700

    apply lazy load for contact, add thumbnail and seo in html

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from 'react-router-dom';
-
+import { useState, useRef, useEffect } from 'react';
 import {
   Navbar,
   About,
@@ -12,6 +12,31 @@ import {
 } from './components';
 
 const App = () => {
+  const [loadContact, setLoadContact] = useState(false);
+  const contactRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          setLoadContact(true);
+          observer.disconnect(); // Stop observing once loaded
+        }
+      },
+      { threshold: 0.1 }
+    );
+
+    if (contactRef.current) {
+      observer.observe(contactRef.current);
+    }
+
+    return () => {
+      if (contactRef.current) {
+        observer.disconnect();
+      }
+    };
+  }, []);
+
   return (
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
@@ -21,16 +46,14 @@ const App = () => {
         </div>
 
         <About />
-
         <Tech />
-
         <Education />
-
         <Works />
-
         <Experience />
 
-        <Contact />
+        <div ref={contactRef}>
+          {loadContact && <Contact />} {/* Load only when in view */}
+        </div>
       </div>
     </BrowserRouter>
   );
