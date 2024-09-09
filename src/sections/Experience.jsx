c6d95a4ed02054ebb55223d1f22c5ef33c85eeae
tainguyencoder <tainguyencoder@gmail.com>
2024-09-09 10:35:29 +0700

    modify some effects

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -24,82 +24,79 @@ const Experience = () => {
   const [animationName, setAnimationName] = useState('idle');
 
   return (
-    <section className="c-space my-20" id="work">
-      <div className="w-full text-white-600">
-        {/* title */}
-        <motion.div variants={textVariant()}>
-          <p className={`${styles.sectionSubText} text-center`}>
-            What I have done
-          </p>
-          <h2 className={`${styles.sectionHeadText} text-center`}>
-            Work Experience
-          </h2>
-        </motion.div>
+    <div className="w-full text-white-600">
+      {/* title */}
+      <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText} text-center`}>
+          What I have done
+        </p>
+        <h2 className={`${styles.sectionHeadText} text-center`}>
+          Work Experience
+        </h2>
+      </motion.div>
 
-        <div className="work-container">
-          {/* 3D model */}
-          <div className="w-full green-pink-gradient p-[1px]">
-            <div className="bg-tertiary xl:h-full lg:h-full md:h-[550px] h-[350px]">
-              <Canvas
-                camera={{
-                  position: [0, 3, 5],
-                  fov: 75,
-                  near: 0.1,
-                  far: 1000,
-                }}
-              >
-                <ambientLight intensity={1} />
-                <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-                <directionalLight position={[10, 10, 10]} intensity={0.5} />
-                <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} />
+      <div className="work-container">
+        {/* 3D model */}
+        <div className="w-full green-pink-gradient p-[1px]">
+          <div className="bg-tertiary xl:h-full lg:h-full md:h-[550px] h-[350px]">
+            <Canvas
+              camera={{
+                position: [0, 3, 5],
+                fov: 75,
+                near: 0.1,
+                far: 1000,
+              }}
+            >
+              <ambientLight intensity={1} />
+              <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+              <directionalLight position={[10, 10, 10]} intensity={0.5} />
+              <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} />
 
-                <Suspense fallback={<CanvasLoader />}>
-                  <group position-y={-3.5} scale={3}>
-                    <Developer animationName={animationName} />
-                  </group>
-                </Suspense>
-              </Canvas>
-              {/* <StarsCanvas /> */}
-
-            </div>
+              <Suspense fallback={<CanvasLoader />}>
+                <group position-y={-4} scale={3.3}>
+                  <Developer animationName={animationName} />
+                </group>
+              </Suspense>
+            </Canvas>
+            {/* <StarsCanvas /> */}
           </div>
-          {/* content */}
-          <div className="work-content">
-            <div className="sm:py-10 py-5 sm:px-5 px-2.5">
-              {workExperiences.map((item, index) => (
-                <div
-                  key={index}
-                  onClick={() => setAnimationName(item.animation.toLowerCase())}
-                  onPointerOver={() =>
-                    setAnimationName(item.animation.toLowerCase())
-                  }
-                  onPointerOut={() => setAnimationName('idle')}
-                  className="work-content_container group"
-                >
-                  <div className="flex flex-col h-full justify-start items-center py-2">
-                    <div className="work-content_logo">
-                      <img className="w-full h-full" src={item.icon} alt="" />
-                    </div>
-
-                    <div className="work-content_bar" />
+        </div>
+        {/* content */}
+        <div className="work-content">
+          <div className="sm:py-10 py-5 sm:px-5 px-2.5">
+            {workExperiences.map((item, index) => (
+              <div
+                key={index}
+                onClick={() => setAnimationName(item.animation.toLowerCase())}
+                onPointerOver={() =>
+                  setAnimationName(item.animation.toLowerCase())
+                }
+                onPointerOut={() => setAnimationName('idle')}
+                className="work-content_container group"
+              >
+                <div className="flex flex-col h-full justify-start items-center py-2">
+                  <div className="work-content_logo">
+                    <img className="w-full h-full" src={item.icon} alt="" />
                   </div>
 
-                  <div className="sm:p-5 px-2.5 py-5">
-                    <p className="font-bold text-white-800">{item.name}</p>
-                    <p className="text-sm mb-5">
-                      {item.pos} -- <span>{item.duration}</span>
-                    </p>
-                    <p className="group-hover:text-white transition-all ease-in-out duration-500">
-                      {item.title}
-                    </p>
-                  </div>
+                  <div className="work-content_bar" />
                 </div>
-              ))}
-            </div>
+
+                <div className="sm:p-5 px-2.5 py-5">
+                  <p className="font-bold text-white-800">{item.name}</p>
+                  <p className="text-sm mb-5">
+                    {item.pos} -- <span>{item.duration}</span>
+                  </p>
+                  <p className="group-hover:text-white transition-all ease-in-out duration-500">
+                    {item.title}
+                  </p>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
