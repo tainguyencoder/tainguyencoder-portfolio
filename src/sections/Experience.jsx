26f68a31034c9f7e19ea226f9e7fca27338dbbf8
tainguyencoder <tainguyencoder@gmail.com>
2024-09-12 00:14:10 +0700

    fix developer smooth

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -14,89 +14,17 @@ import CanvasLoader from '../components/CanvasLoader.jsx';
 // data
 import { workExperiences } from '../constants';
 
-const ExperienceCardPopup = ({ experience, onClose }) => {
-  const popupRef = useRef();
-
-  // Handle Esc key press
-  useEffect(() => {
-    const handleKeyDown = (event) => {
-      if (event.key === 'Escape') {
-        onClose();
-      }
-    };
-
-    window.addEventListener('keydown', handleKeyDown);
-    return () => window.removeEventListener('keydown', handleKeyDown);
-  }, [onClose]);
-
-  // Handle click outside of the popup
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (popupRef.current && !popupRef.current.contains(event.target)) {
-        onClose();
-      }
-    };
-
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => document.removeEventListener('mousedown', handleClickOutside);
-  }, [onClose]);
-
-  const variants = {
-    hidden: { opacity: 0, scale: 0.9, y: 50 }, // Initial state
-    visible: { opacity: 1, scale: 1, y: 0 }, // Final state
-    exit: { opacity: 0, scale: 0.9, y: 50 }, // State when exiting
-  };
-
-  return (
-    <motion.div
-      className="fixed inset-0 flex items-center justify-center mx-10 z-50"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      variants={variants}
-      transition={{
-        type: 'tween',
-        delay: 0.2,
-        duration: 0.8,
-        ease: 'easeOut',
-      }}
-    >
-      <div ref={popupRef} className="popup-content group">
-        <div className="flex flex-col justify-start items-center py-2">
-          <div className="popup-content_logo">
-            <img className="w-12 h-12" src={experience.icon} alt="" />
-          </div>
-        </div>
-
-        <div className="sm:p-5 px-2.5 py-5">
-          <p className="font-bold text-white-100">{experience.name}</p>
-          <p className="text-md mb-5">
-            <span>{experience.duration}</span>
-          </p>
-          <ul className="mt-5 list-disc ml-5 space-y-2">
-            {experience.points.map((point, index) => (
-              <li
-                key={`experience-point-${index}`}
-                className="text-cyan-400 text-[11px] xs:text-[16px] sm:text-[16px] sm:font-medium pl-1 tracking-wider"
-              >
-                {point}
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-    </motion.div>
-  );
-};
-
 const Experience = () => {
   const [animationName, setAnimationName] = useState('idle');
   const [items, setItems] = useState(workExperiences);
   const [selectedId, setSelectedId] = useState(null);
+  const popupRef = useRef();
+
   const handleClosePopup = () => setSelectedId(null);
 
   useEffect(() => {
     const handleScroll = () => {
+      setAnimationName('idle')
       setSelectedId(null);
     };
     window.addEventListener('scroll', handleScroll);
@@ -104,6 +32,35 @@ const Experience = () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === 'Escape') {
+        setAnimationName('idle');
+        handleClosePopup();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, []);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (popupRef.current && !popupRef.current.contains(event.target)) {
+        handleClosePopup();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, []);
+
+  const popupVariants = {
+    hidden: { opacity: 0, scale: 0.5, borderRadius: '50%' },
+    visible: { opacity: 1, scale: 1, borderRadius: '10px' },
+    exit: { opacity: 0, scale: 0.5, borderRadius: '50%' },
+  };
 
   return (
     <div className="w-full text-white-600">
@@ -178,10 +135,43 @@ const Experience = () => {
         </div>
         <AnimatePresence>
           {selectedId && (
-            <ExperienceCardPopup
-              experience={items.find((item) => item.id === selectedId)}
-              onClose={handleClosePopup}
-            />
+            <motion.div
+              className="fixed inset-0 flex items-center justify-center mx-10 z-50"
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+              variants={popupVariants}
+              transition={{
+                type: 'tween',
+                duration: 0.6,
+                ease: 'easeOut',
+              }}
+            >
+              <div ref={popupRef} className="popup-content group">
+                <div className="flex flex-col justify-start items-center py-2">
+                  <div className="popup-content_logo">
+                    <img className="w-12 h-12" src={items.find((item) => item.id === selectedId).icon} alt="" />
+                  </div>
+                </div>
+
+                <div className="sm:p-5 px-2.5 py-5">
+                  <p className="font-bold text-white-100">{items.find((item) => item.id === selectedId).name}</p>
+                  <p className="text-md mb-5">
+                    <span>{items.find((item) => item.id === selectedId).duration}</span>
+                  </p>
+                  <ul className="mt-5 list-disc ml-5 space-y-2">
+                    {items.find((item) => item.id === selectedId).points.map((point, index) => (
+                      <li
+                        key={`experience-point-${index}`}
+                        className="text-cyan-400 text-[11px] xs:text-[16px] sm:text-[16px] sm:font-medium pl-1 tracking-wider"
+                      >
+                        {point}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            </motion.div>
           )}
         </AnimatePresence>
       </div>
